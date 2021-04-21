fa6f855855f76b1a6f54c1b7597bf28ab7d610ab
martin.osikovsky <osikovsky@gmail.com>
2021-04-21 22:08:04 +0200

    Additional settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,49 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../mocks');
+}
+
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  
   * {
-    font-family: 'Open Sans', sans-serif;
+   box-sizing: border-box;
+  }
+  
+  .mr-sm {
+    margin-right: 10px;
+  }
+  
+  .mb-sm {
+    margin-bottom: 20px;
+  }
+  
+  .mb-lg {
+    margin-bottom: 60px;
   }
 `;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    brand: '#ed6b21',
+    white: '#fff',
+    gray: {
+      300: '#f6f6f6',
+      400: '#e2e3e4',
+      500: '#b0afaf',
+    },
+  },
+  breakpoints: {
+    xs: '375px',
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
   },
 };
 
